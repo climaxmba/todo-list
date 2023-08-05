78834ed8e3cb6d675549684afb3860501d4f28dc
Climax Mba <climaxium150@gmail.com>
2023-08-05 14:32:48 +0100

    Fix <li> 'double nesting' in projectspage

--- src/modules/display.js
+++ src/modules/display.js
@@ -305,8 +305,8 @@ function renderData(data) {
 
     for (let j = 0; j < tasks.length; j++) {
       const taskNode = document.createElement("li");
-      taskNode.innerHTML = `<li class="flex-list-item">
-        <span class="flex-list-elem"><span class="task-name"></span><span class="task-lbl"></span></span>
+      taskNode.className = "flex-list-item"
+      taskNode.innerHTML = `<span class="flex-list-elem"><span class="task-name"></span><span class="task-lbl"></span></span>
         <span data-project="${i}" data-task="${j}" class="flex-list-elem">
             <svg data-action-type="view-task" class="actions" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <title>View info</title>
@@ -320,8 +320,7 @@ function renderData(data) {
               <title>Delete</title>
               <path d="M14.12,10.47L12,12.59L9.87,10.47L8.46,11.88L10.59,14L8.47,16.12L9.88,17.53L12,15.41L14.12,17.53L15.53,16.12L13.41,14L15.53,11.88L14.12,10.47M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9Z" />
             </svg>
-        </span>
-      </li>`;
+        </span>`;
       taskNode.querySelector(".task-name").textContent = tasks[j].title;
       taskNode.querySelector(".task-lbl").textContent = tasks[j].priority;
       projectNode.querySelector("ul").appendChild(taskNode);
